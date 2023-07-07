4211ae5ec605f650fe4d58e53aedfb19677eac91
Supriya Pandurangacharya Upadhyaya <supriya.upadhyaya@st.ovgu.de>
2023-07-07 08:54:24 +0200

    Show offers in product listing page and search resutls page

--- backend/controller.py
+++ backend/controller.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from backend.model import readOperation, searchProductList, readOperationCategory, readOperationCategoryImages, \
-                          readOperationSubCategory, readOperationProduct, readOperationProductRatings
+                          readOperationSubCategory, readOperationProduct, readOperationProductRatings, readOffers
 
 def getAllCategoriesList():
     data = {}
@@ -56,6 +56,18 @@ def getSearch(query):
             block["product_price"] = row["product_price"]
             block["sponsored"] = row["sponsored"]
             block["url"] = url
+            block['offer_flag'] = row["offer_flag"]
+            if block['offer_flag'] != 0:
+                offers = readOffers(block['product_serial_number'], row['offer_id'])
+                for item in offers:
+                    block['offer_percent'] = item['offer_percent']
+                    block['offer_image_id'] = item['offer_image_id']
+                    offer_price = round((block['product_price'] - (block['product_price'] * block['offer_percent']) / 100), 2)
+                    block['offer_price'] = offer_price
+            else:
+                block['offer_percent'] = 0
+                block['offer_image_id'] = 0
+                block['offer_price'] = 'None'
             products[i] = block
             i += 1
         return products
--- backend/controllers/product.py
+++ backend/controllers/product.py
@@ -215,6 +215,18 @@ def getspecialcategory(id):
         block["product_price"] = row["product_price"]
         block["sponsored"] = row["sponsored"]
         block["url"] = url
+        block['offer_flag'] = row["offer_flag"]
+        if block['offer_flag'] != 0:
+            offers = readOffers(block['product_serial_number'], row['offer_id'])
+            for item in offers:
+                block['offer_percent'] = item['offer_percent']
+                block['offer_image_id'] = item['offer_image_id']
+                offer_price = round((block['product_price'] - (block['product_price'] * block['offer_percent']) / 100), 2)
+                block['offer_price'] = offer_price
+        else:
+            block['offer_percent'] = 0
+            block['offer_image_id'] = 0
+            block['offer_price'] = 'None'
         products[i] = block
         i += 1
     return products
